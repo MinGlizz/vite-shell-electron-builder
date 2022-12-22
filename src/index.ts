8fe6248f37ca41d2270d4af3193bfbdc1f3b33e4
Ming <>
2022-12-22 13:47:56 +0800

    feat: build

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import type { PluginOption } from 'vite'
+import EleMain, { eleShellBuild } from './main'
+
+export default function vitePluginTemplate(
+  _configEle: eleShellBuild | undefined = {
+    dirName: 'app',
+    dependencies: [],
+    entry: 'dist',
+    outDir: 'build',
+  },
+): PluginOption {
+  const eleBuild = new EleMain(_configEle)
+  return {
+    name: 'vite-plugin-template',
+    enforce: 'pre', // post
+    apply: 'build',
+    config() {
+      eleBuild.init()
+    },
+    closeBundle() {
+      eleBuild.mainBuilds()
+    },
+  }
+}
